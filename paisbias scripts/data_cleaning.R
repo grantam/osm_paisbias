3f7660d2a58bd21f049a5d0d8823441e5e418beb
grantam <141366794+grantam@users.noreply.github.com>
2024-06-16 20:12:41 -0600

    recleaning data

--- paisbias scripts/data_cleaning.R
+++ paisbias scripts/data_cleaning.R
@@ -15,10 +15,22 @@ library("ggpubr")
 library("ggeffects")
 library("patchwork")
 library("ggExtra")
+library("vdemdata")
 
 
 ## Load the data
-## To ensure that the data meet the requriements of other scholar, the data is set to the specification of the orignal analysis: "We are only looking at countries since 1900, and need to combine FH and prepare Polity2. We also only care about sovereign countries."
+## To ensure that the data meet the requirements of other scholar, the data uses the variables in the orignal analysis
+
+df_vdem <- vdem %>%
+  select(country_name, COWcode, v2x_polyarchy, v2ellovtlg, v2ellovtsm, v2ellostsl, v2ellostss, v2ellostts, v2elvotlrg, v2elvotsml, v2xex_elecreg, v2xlg_elecreg, v2x_suffr, v2eltype_0, v2eltype_6, v2ellocons, v2ellocumul, v2ellocons, v2elprescons, v2elprescumul, v2elturnhog, v2elturnhos, v2eltvrexo) %>%
+  mutate(v2elturnhog = as.factor(v2elturnhog), v2elturnhos = as.factor(v2elturnhos), v2eltvrexo = as.factor(v2eltvrexo), )
+  
+  
+
+
+
+
+
 
 shared_polities <- read_csv("og_rep/shared_polities.csv")
 shared_polities <- as.vector(shared_polities$country_text_id)
@@ -37,5 +49,6 @@ df_backsliding_all <-
 df_backsliding <- 
   df_backsliding_all |>
   ungroup() |>
-  dplyr::select(country_name, country_id, year, v2x_polyarchy, e_fh_combined, e_polity2, v2ellovtlg, v2ellovtsm, v2ellovttm, v2ellostsl, v2ellostss, v2ellostts, v2elvotlrg, v2elvotsml, v2xex_elecreg, v2xex_elecreg, multi_party_legislative_elections, v2x_suffr, v2eltype_legislative, v2eltype_presidential, top2_difference, top2_combined, top2_monopoly, v2ellocons, v2ellocumul, v2ellocons, v2elprescumul, v2elturnhog, v2elturnhos, v2eltvrexo, turnover_period, turnover_event, two_turnover_period)
+  dplyr::select(country_name, country_id, year, v2x_polyarchy, e_fh_combined, e_polity2, v2ellovtlg, v2ellovtsm, v2ellovttm, v2ellostsl, v2ellostss, v2ellostts, v2elvotlrg, v2elvotsml, v2xex_elecreg, v2xex_elecreg, multi_party_legislative_elections, v2x_suffr, v2eltype_legislative, v2eltype_presidential, top2_difference, top2_combined, top2_monopoly, v2ellocons, v2ellocumul, v2ellocons, v2elprescumul, v2elturnhog, v2elturnhos, v2eltvrexo, turnover_period, turnover_event, two_turnover_period) %>%
+  mutate(ctyid = as.factor(country_name))
 
