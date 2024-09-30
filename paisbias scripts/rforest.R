c4ca9b918ecede202a65355b2c0873b55af27fe8
Grant Arney Mitchell <MitchellG@ceu.edu>
2024-09-30 15:30:54 +0200

    add my own random forest

--- paisbias scripts/rforest.R
+++ paisbias scripts/rforest.R
@@ -54,7 +54,7 @@ data <- vdem %>%
   mutate(turnover_test = ifelse(v2elturnhog == 0, 0, 1), COWcode = as.factor(COWcode), election = ifelse(is.na(v2eltype_0), 0, 1)) %>%
   replace_na(replace = list(turnover_test = 0, v2elturnhog = 0)) %>%
   mutate(v2elturnhog = as.factor(v2elturnhog), fix = as.factor(country_name)) %>%
-  select(turnover_test, v2x_api, v2x_libdem, election, COWcode, v2elturnhog, year, fix, v2ellostsl, v2elsuffrage, e_p_polity, v2expathhg, v2ex_elechog, v2ex_elechos, v2exhoshog, ifs = country_text_id, v2expathhg, v2expathhs, v2x_freexp_altinf, v2xel_frefair, country_id) %>%
+  select(turnover_test, v2x_polyarchy, v2x_libdem, election, COWcode, v2elturnhog, year, fix, v2ellostsl, v2elsuffrage, e_p_polity, v2expathhg, v2ex_elechog, v2ex_elechos, v2exhoshog, ifs = country_text_id, v2expathhg, v2expathhs, v2x_freexp_altinf, v2xel_frefair, country_id) %>%
   group_by(COWcode) %>%
   mutate(last_elect = log(neg_binom(election) + 1), var_elect = var(last_elect), exec_chose = ifelse(v2exhoshog == 1, v2expathhs, v2expathhg-1), parli = ifelse(exec_chose == 6, 1, 0))
 
@@ -112,29 +112,39 @@ dat <- data %>%
          last_elect, competitive_elections,
          v2elsuffrage,
          political_liberties,
-         v2x_api, year, COWcode) %>%
+         v2x_polyarchy, year, COWcode, countryn) %>%
   na.omit()
 
 
-X <- dat %>%
-  select(turnover_event,
-         legislative_elections,
-         executive_elections,
-         `multi-party_legislative_elections`,
-         last_elect, competitive_elections,
-         v2elsuffrage,
-         political_liberties)
+REmat <- matrix(NA, nrow = 196, ncol = 200)
 
-list_dat <- list(
-  X = X,
-  Y = as.numeric(pull(dat, v2x_api)),
-  id = as.numeric(pull(dat, COWcode)),
-  Z = as.matrix(rep(1, nrow(dat))),
-  time = as.numeric(pull(dat, year)))
+for (i in 1:200) {
+  
+  variable_to_select <- dat %>%
+  select(-COWcode, -year, -v2x_polyarchy, -countryn) %>%
+  colnames()
+  
+  variables <- sample(variable_to_select, 3, replace = FALSE)
+  
+  sampled_data <- dat %>%
+    group_by(COWcode) %>%
+    sample_n(size = n(), replace = TRUE) %>%
+    arrange(COWcode, year) %>%
+    mutate(n_in_group = row_number()) %>%
+    ungroup()
+  
+  X <- sampled_data %>%
+    select(all_of(variables))
+  
+  list_dat <- list(
+    X = as.matrix(X),
+    Y = as.numeric(pull(sampled_data, v2x_polyarchy)),
+    id = as.numeric(pull(sampled_data, COWcode)),
+    Z = as.matrix(rep(1, nrow(sampled_data))),
+    time = as.numeric(pull(sampled_data, n_in_group)))
 
 
-mert1 <- 
-  MERF(
+  mert1 <- LongituRF::MERT(
     X = data.frame(list_dat$X),
     Y = list_dat$Y,
     id = list_dat$id,
@@ -142,7 +152,51 @@ mert1 <-
     time = list_dat$time,
     sto = 'OrnUhl')
 
-mert1$random_effects
+REmat[, i] <- mert1$random_effects
 
+}
+
+
+REmat <- as.matrix(REmat)
+
+REmat <- t(REmat)
+
+REmat <- as.data.frame(REmat)
+
+summarize_all()
+
+data_viz <- REmat %>%
+  summarize_all(list(
+    lower = ~quantile(., probs = 0.025, na.rm = TRUE),
+    mean = ~mean(., na.rm = TRUE),
+    upper = ~quantile(., probs = 0.975, na.rm = TRUE)
+  ))
+
+# Pivot to long format
+data_viz_long <- data_viz %>%
+  pivot_longer(cols = everything(), 
+               names_to = c("variable", ".value"),
+               names_sep = "_") %>%
+  mutate(problem = ifelse(mean > upper | mean < lower, FALSE, TRUE),
+         mean = as.numeric(mean),
+         lower = as.numeric(lower),
+         upper = as.numeric(upper),
+         sig = ifelse((lower > 0 & upper > 0 | lower < 0 & upper < 0), T, F))
+
+uniquecow <- dat %>%
+  group_by(COWcode) %>%
+  summarise(name = first(countryn))
+
+data_viz_long <- cbind(data_viz_long, uniquecow) %>%
+  arrange(lower)  # Sort by mean in ascending order
+
+# Plot
+ggplot(data = data_viz_long) +
+  geom_linerange(aes(xmin = lower, xmax = upper, y = reorder(name, mean), color = sig)) +  # Reorder variables based on mean
+  geom_point(aes(x = mean, y = reorder(name, mean), color = sig)) +  # Reorder variables based on mean
+  geom_vline(xintercept = 0, color = "darkblue") +
+  theme_bw() +
+  coord_flip() +
+  theme(axis.text.x = element_text(angle = 90))
 
 
